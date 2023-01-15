89bc8d04f6ac3238e103b237fc48a82f6a3f8dfb
Samuellmk <samuel_leong@outlook.com>
2023-01-15 23:59:33 +0800

    Post Feed

--- quasar.config.js
+++ quasar.config.js
@@ -34,7 +34,7 @@ module.exports = configure(function (/* ctx */) {
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
-      'ionicons-v4',
+      // 'ionicons-v4',
       // 'mdi-v5',
       // 'fontawesome-v6',
       // 'eva-icons',
@@ -44,6 +44,7 @@ module.exports = configure(function (/* ctx */) {
 
       'roboto-font', // optional, you are not bound to it
       'material-icons', // optional, you are not bound to it
+      'material-icons-outlined',
     ],
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
