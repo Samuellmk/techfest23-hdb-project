ce7f48a702177319c729fecfa6bff849caf6ac89
Li Zhaoyuan <lizhaoyuan123@hotmail.com>
2023-01-15 20:17:19 +0800

    Create CreatePostDialog.vue

---
+++ src/components/CreatePostDialog.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <q-dialog
+      v-model="dialog"
+      persistent
+      maximized
+      transition-show="slide-up"
+      transition-hide="slide-down"
+    >
+      <q-card class="bg-primary text-white">
+        <q-bar>
+          <q-space />
+
+          <q-btn dense flat icon="close" v-close-popup>
+            <q-tooltip class="bg-white text-primary">Close</q-tooltip>
+          </q-btn>
+        </q-bar>
+
+        <q-card-section>
+          <div class="text-h6">Create New Fault Post</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+          repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis
+          perferendis totam, ea at omnis vel numquam exercitationem aut, natus
+          minima, porro labore.
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps(['dialog']);
+const emits = defineEmits(['update:dialog']);
+
+const dialog = computed({
+  get() {
+    return props.dialog;
+  },
+  set(value: boolean) {
+    emits('update:dialog', value);
+  },
+});
+</script>
