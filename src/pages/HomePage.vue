e521580d9a71b9ef3e210dbac933dc1bbd4cd2d6
Samuellmk <samuel_leong@outlook.com>
2023-01-17 02:32:16 +0800

    got refresh now

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,19 +1,24 @@
 <template>
   <q-page class="items-center justify-evenly">
-    <q-list v-for="post in posts" :key="post.id">
-      <q-item clickable v-ripple class="no-padding" @click="onPostClick(post)">
-        <post-component :post="post" class="fit" />
-      </q-item>
-      <q-separator />
-    </q-list>
+    <q-pull-to-refresh @refresh="refresh">
+      <q-list v-for="post in posts" :key="post.id">
+        <q-item
+          clickable
+          v-ripple
+          class="no-padding"
+          @click="onPostClick(post)"
+        >
+          <post-component :post="post" class="fit" />
+        </q-item>
+        <q-separator />
+      </q-list>
+    </q-pull-to-refresh>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import PostComponent from 'components/PostComponent.vue';
-import { PostModel } from 'src/components/models';
 import { useRouter } from 'vue-router';
-import MOCK_POSTS from './POST_DATA.json';
 import PocketBase from 'pocketbase';
 import { PBPostModel } from './creation-interface';
 import { onMounted, Ref, ref } from 'vue';
@@ -21,6 +26,11 @@ import { onMounted, Ref, ref } from 'vue';
 const pb = new PocketBase(process.env.VITE_POCKETBASE_URL);
 
 const router = useRouter();
+
+const refresh = async (done: any) => {
+  await fetchPosts();
+  done();
+};
 
 const onPostClick = (postObj: PBPostModel) => {
   router.push({
@@ -30,11 +40,16 @@ const onPostClick = (postObj: PBPostModel) => {
 };
 const posts: Ref<PBPostModel[]> = ref([]);
 //const posts = MOCK_POSTS;
-onMounted(async () => {
+
+const fetchPosts = async () => {
   posts.value = (await pb
     .collection('posts')
     .getFullList(200 /* batch size */, {
       sort: '-created',
     })) as PBPostModel[];
+};
+
+onMounted(async () => {
+  fetchPosts();
 });
 </script>
