bff9ae373e16766af0fb65b6ba490644d685f99d
erictan23 <eric23july@hotmail.com>
2023-01-16 20:31:26 +0800

    Added different vouchers for redemptions

---
+++ src/pages/redemptions.ts
@@ -0,0 +1,38 @@
+export const redemptions = [
+  {
+    discountName: '$5 Dollar Season Parking Discount',
+    pointsDeduct: 50,
+    color: 'teal-12',
+    url: 'https://qrco.de/bddM4R',
+  },
+  {
+    discountName: '$10 Dollar Season Parking Discount',
+    pointsDeduct: 75,
+    color: 'cyan-6',
+    url: 'https://qrco.de/bddM4R',
+  },
+  {
+    discountName: '$15 Dollar Season Parking Discount',
+    pointsDeduct: 125,
+    color: 'light-green-2',
+    url: 'https://qrco.de/bddM4R',
+  },
+  {
+    discountName: '$20 Dollar Season Parking Discount',
+    pointsDeduct: 150,
+    color: 'yellow-1',
+    url: 'https://qrco.de/bddM4R',
+  },
+  {
+    discountName: '$10 CDC Discount',
+    pointsDeduct: 75,
+    color: 'light-blue-2',
+    url: 'https://qrco.de/bddM4R',
+  },
+  {
+    discountName: '$20 CDC Discount',
+    pointsDeduct: 150,
+    color: 'amber-11',
+    url: 'https://qrco.de/bddM4R',
+  },
+];
